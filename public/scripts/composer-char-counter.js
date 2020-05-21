09f3e16c4dcdd0babd87654f5934c1ad1f9f27c7
Michael Hallett <m.hallett@gmail.com>
2020-05-21 19:25:58 +0000

    character counter script for new tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").on("keyup", function(event) {
+    const counter = $(this).siblings().find(".counter")[0];
+    const currentCharCount = $(this).val().length;
+
+    counter.innerText =  140 - currentCharCount;
+    
+    if (counter.innerText < 0) {
+      $(counter).addClass("charOverLimit");
+    } else {
+      $(counter).removeClass("charOverLimit");
+
+    }
+  })
+});
