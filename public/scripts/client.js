b7dafb8cdfd93c6832436b4c13887df0305b5854
Michael Hallett <m.hallett@gmail.com>
2020-05-21 19:31:39 +0000

    Added clientside logic for creating and displaying tweets through Jquery and Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ $(document).ready(function() {
+  
+
+  // loops through the passed tweet object and posts the formatted tweets to the tweet container in reverse chronological order.
+   const renderTweets = (tweets) => {
+     tweets.reverse();
+     $.each(tweets, function(index, tweet) {
+       $('.tweet-container').append(createTweetElement(tweet));
+     });
+   };
+
+   const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  // const escape =  function(str) {
+  //   let newText = document.createTextNode(str);
+  //   console.log(newText)
+  //   return newText;
+  // }
+
+  // creates the tweet html for passed tweet information
+  const createTweetElement = (tweet) => {
+    const $tweet = $(`
+    <article class="tweet">
+    <header>
+      <img src="${tweet.user.avatars}">
+      <span>${tweet.user.name}</span>
+      <div class="hidden">
+        <p>${tweet.user.handle}</p>
+      </div>
+    </header>
+    <div class="tweet-content">
+      <p>${escape(tweet.content.text)}</p>
+    </div>
+    <footer>
+      <p>${moment(tweet.created_at).fromNow()}</p>
+      <div class="hidden icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+      <div>
+    </footer>
+  </article>
+  `);
+
+  return $tweet;
+  };
+
+  $("#submit-tweet").submit(function( event ) {
+    event.preventDefault();
+    const $charCount = $(this).children($('textarea'))[1].value.length;
+    if ($charCount === 0) {
+      $('.error-empty').slideDown()
+    } else if ($charCount > 140) {
+      $('.error-overLimit').slideDown()
+    } else {
+      $('.error-empty').slideUp()
+      $('.error-overLimit').slideUp()
+      $.ajax({
+        url: '/tweets', 
+        method: 'POST',
+        data: $(this).serialize()
+      })
+      .done(function() {
+        $('.tweet-container').empty();
+        loadTweets()
+      });
+    } 
+  });
+
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+    })
+    .done(function(tweets){
+      console.log('get finished!');
+      renderTweets(tweets);
+    })
+  };
+
+  loadTweets();
+ 
+ });
